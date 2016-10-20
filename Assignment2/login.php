751b9cd93f091b2369e242228eb7ddfebf635df4
Sergio Sierra <sdmunozsierra@miners.utep.edu>
2016-10-20 11:52:00 -0600

    Update login.php
    Added session

--- Assignment2/login.php
+++ Assignment2/login.php
@@ -1,4 +1,34 @@
-//This is the information to connect to the database
 <?php
-
+// Start the session
+session_start();
 ?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Set session variables
+$_SESSION["favcolor"] = "green";
+$_SESSION["favanimal"] = "cat";
+echo "Session variables are set.";
+?>
+
+// create a form with fields username and password
+<form action="login.php" method="post">
+Username: <input type="text" name="username"><br>
+Password: <input type="text" name="password"><br>
+<input type="submit">
+</form>
+  
+  
+  
+<?php
+// remove all session variables
+session_unset();
+
+// destroy the session
+session_destroy();
+?>
+
+</body>
+</html> 

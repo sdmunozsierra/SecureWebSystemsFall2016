541181b74c35572f80a839073bb554fd373185e8
Sergio Sierra <sdmunozsierra@miners.utep.edu>
2016-10-25 22:34:58 -0600

    Update admin.php

--- Assignment2/admin.php
+++ Assignment2/admin.php
@@ -1,3 +1,17 @@
-<php?
+<?php //admin.php
+/**
+ * All pages should have a sign in button if the visitor is not signed in, and a sign out button otherwise.
+ * All pages should have links to the other accessible pages, and no link to not accessible pages.
+ * All pages should have some text in it indicating where we are.
+ *
+ * If the visitor is logged in as an administrator, all pages should be accessible.
+ *
+ * For the admin account, create an account with admin as user name and nimda339 as password.
+ * This will be the only administrator account.
+ */
 
-?>
+echo "This is the admin page";
+//add sign in button
+
+//if sign in add logout button
+

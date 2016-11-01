d7ff8e2dc72c77af5818bf326b355d679e93c147
Sergio Sierra <sdmunozsierra@miners.utep.edu>
2016-11-01 15:31:20 -0600

    Update user.php

--- Assignment2/user.php
+++ Assignment2/user.php
@@ -12,3 +12,22 @@ echo "<h1>Countdown until Christmas</h1><br>";
 echo '<script type="text/javascript" src="countdown.js"></script>';
 
 ?>
+
+//ADD THE FOLLOWING
+<?php // continue.php
+  session_start();
+  if (isset($_SESSION['username']))
+  {
+    $username = $_SESSION['username'];
+    $password = $_SESSION['password'];
+    $forename = $_SESSION['forename'];
+    $surname  = $_SESSION['surname'];
+    echo "Welcome back $forename.<br>
+          Your full name is $forename $surname.<br>
+          Your username is '$username'
+          and your password is '$password'.";
+  }
+  else echo "Please <a href='authenticate2.php'>cli
+ck here</a> to log 
+in.";
+?>
